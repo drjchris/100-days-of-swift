090476f932656f442c5406df573e1732019fb2be
Jose Christian <j.christian@brighton.ac.uk>
2022-06-24 17:56:41 +0700

    another restructure

--- Day-2/main.swift
+++ Day-2/main.swift
@@ -7,4 +7,14 @@
 
 // Day two - Creating a structure and parsing some json
 
+// create a structure for each json entry
+struct personel{
+    let name: String
+    let surname: String
+    let age: Int
+    let occupation: String
+}
+
 import Foundation
+
+print("this works i guess")
